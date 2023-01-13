db62119ffa0173664594aab3641a8f0c8c8d9e12
tkmcnc <kurbanovkovus42@gmail.com>
2023-01-13 22:16:42 +0300

    first commit

---
+++ resources/views/app/footer.blade.php
@@ -0,0 +1,27 @@
+<div class="container">
+    <div>
+        <div class="col">
+            <p class="h1">Get in touch -</p>
+            <p>"Töleg" programmasynyň üsti bilen telekom internet <br> hasabyňyzy doldurmak aňsat.</p>
+            <div>
+                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M4.16667 3.33331H7.5L9.16667 7.49998L7.08333 8.74998C7.9758 10.5596 9.44039 12.0242 11.25 12.9166L12.5 10.8333L16.6667 12.5V15.8333C16.6667 16.2753 16.4911 16.6993 16.1785 17.0118C15.866 17.3244 15.442 17.5 15 17.5C11.7494 17.3024 8.68346 15.9221 6.38069 13.6193C4.07792 11.3165 2.69754 8.25059 2.5 4.99998C2.5 4.55795 2.67559 4.13403 2.98816 3.82147C3.30072 3.50891 3.72464 3.33331 4.16667 3.33331Z" stroke="#4077EF" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M12.5 5.83331C12.942 5.83331 13.366 6.00891 13.6785 6.32147C13.9911 6.63403 14.1667 7.05795 14.1667 7.49998" stroke="#4077EF" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M12.5 2.5C13.8261 2.5 15.0979 3.02678 16.0355 3.96447C16.9732 4.90215 17.5 6.17392 17.5 7.5" stroke="#4077EF" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+                <p class="d-inline text-secondary">+993 65 72 61 66</p>
+            </div>
+            <div class="mt-1">
+                <svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M17 5L9.5 10L2 5" stroke="#4077EF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                    <path d="M3.5 3H15.5C16.325 3 17 3.73125 17 4.625V14.375C17 15.2688 16.325 16 15.5 16H3.5C2.675 16 2 15.2688 2 14.375V4.625C2 3.73125 2.675 3 3.5 3Z" stroke="#4077EF" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"/>
+                </svg>
+                <p class="d-inline text-secondary">contact@belgilabs.com</p>
+            </div>
+        </div>
+        <div class="col">
+            <input type="email" placeholder="email">
+            <input type="text" name="" id="" placeholder="message">
+        </div>
+    </div>
+</div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,35 +1,25 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>@yield('title')</title>
+    <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/bootstrap-icons.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/splide.min.css') }}">
+    <link rel="stylesheet" href="{{asset('css/font.css')}}">
+</head>
+<body class="bg-light">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+@include('app.nav')
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap">
+@yield('content')
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100">
-            @include('layouts.navigation')
+@include('app.footer')
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+<script type="text/javascript" src="{{ asset('js/bootstrap.bundle.min.js') }}"></script>
+<script type="text/javascript" src="{{ asset('js/splide.min.js') }}"></script>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
-    </body>
+</body>
 </html>
